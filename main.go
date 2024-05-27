e6dd53db4de5d43bc143da2d0ea00e36a14b9ca6
Revan <Revan_TSL@hotmail.com>
2024-05-27 15:42:24 +0200

    refactoring wip -> restructuring code

--- main.go
+++ main.go
@@ -1,17 +1,14 @@
 package main
 
-import (
-	"lh-toolkit/internal/client"
-	"lh-toolkit/internal/server"
-	"os"
-)
+import "fmt"
 
 func main() {
-	args := os.Args
-	isServer := args[1]
-	if isServer == "server" {
-		server.AppServer()
-	} else {
-		client.AppClient()
-	}
+	wait := make(chan struct{})
+	<-wait
+}
+
+//export add
+func add(x, y int) int {
+	fmt.Print("im adding two numbers")
+	return x + y
 }
---
+++ preview.js
@@ -0,0 +1,16 @@
+// WEBRTC
+// async function captureScreen() {
+//     let mediaStream = null;
+//     try {
+//         mediaStream = await navigator.mediaDevices.getDisplayMedia({
+//             video: {
+//                 cursor: "always"
+//             },
+//             audio: false
+//         });
+//         console.log(mediaStream)
+//     } catch (ex) {
+//         console.log("Error occurred", ex);
+//     }
+// }
+// captureScreen();
