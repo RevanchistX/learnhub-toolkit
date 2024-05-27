837c031cd57d749e826232a9d8caee0e1b78d509
Revan <Revan_TSL@hotmail.com>
2024-05-27 16:06:41 +0200

    create room share

--- internal/server/server.go
+++ internal/server/server.go
@@ -1,80 +1,103 @@
 package server
 
 import (
-	"bytes"
+	"encoding/json"
 	"fmt"
-	"github.com/gorilla/websocket"
-	"github.com/kbinani/screenshot"
-	"image"
-	"image/png"
-	"net"
 	"net/http"
+	"net/url"
 )
 
-func AppServer() {
-	setupRoutes()
-	ip := GetOutboundIP()
-	fmt.Println(ip)
-	http.ListenAndServe(":8080", nil)
-}
-
-func GetOutboundIP() net.IP {
-	addrs, err := net.InterfaceAddrs()
-	if err != nil {
-		fmt.Println(err)
-	}
-	for _, address := range addrs {
-		// check the address type and if it is not a loopback the display it
-		if ipnet, ok := address.(*net.IPNet); ok && !ipnet.IP.IsLoopback() {
-			if ipnet.IP.To4() != nil {
-				return ipnet.IP
-			}
-		}
-	}
-	return nil
+type Room struct {
+	name string
+	url  url.URL
 }
 
 func setupRoutes() {
-	http.HandleFunc("/streaming", streaming)
-	http.HandleFunc("/wasm", wasm)
+	//http.HandleFunc("/streaming", streaming)
+	//http.HandleFunc("/wasm", wasm)
+	http.HandleFunc("/create-room", createRoom)
 }
 
-func wasm(writer http.ResponseWriter, request *http.Request) {
+func createRoom(writer http.ResponseWriter, request *http.Request) {
 	writer.Header().Set("Access-Control-Allow-Origin", "*")
-	http.ServeFile(writer, request, "./assets/simple.wasm")
-}
+	writer.Header().Set("Content-Type", "application/json")
 
-var upgrader = websocket.Upgrader{
-	ReadBufferSize:  1024,
-	WriteBufferSize: 1024,
-	CheckOrigin:     func(r *http.Request) bool { return true },
-}
-
-func streaming(writer http.ResponseWriter, request *http.Request) {
-	ws, err := upgrader.Upgrade(writer, request, nil)
+	room := Room{
+		name: "room-1",
+		url: url.URL{
+			Scheme: "https",
+			Host:   "toolkit.vercel.app",
+		},
+	}
+	roomJson, err := json.Marshal(room)
 	if err != nil {
-		fmt.Println(err)
+		fmt.Print("cannot generate room json")
 	}
-	for true {
-		wsErr := ws.WriteMessage(websocket.BinaryMessage, encodeImage())
-		if wsErr != nil {
-			fmt.Println("am i throwing errors")
-			fmt.Println(err)
-		}
-	}
+	writer.Write(roomJson)
 }
 
-func generateImage(index int) image.Image {
-	bounds := screenshot.GetDisplayBounds(index)
-	capturedImg, err := screenshot.CaptureRect(bounds)
-	if err != nil {
-		panic(err)
-	}
-	return image.Image(capturedImg)
+func AppServer() {
+	setupRoutes()
+	//ip := GetOutboundIP()
+	//fmt.Println(ip)
+	http.ListenAndServe(":8080", nil)
 }
 
-func encodeImage() []byte {
-	buffer := new(bytes.Buffer)
-	png.Encode(buffer, generateImage(1))
-	return buffer.Bytes()
-}
+//
+//func GetOutboundIP() net.IP {
+//	addrs, err := net.InterfaceAddrs()
+//	if err != nil {
+//		fmt.Println(err)
+//	}
+//	for _, address := range addrs {
+//		// check the address type and if it is not a loopback the display it
+//		if ipnet, ok := address.(*net.IPNet); ok && !ipnet.IP.IsLoopback() {
+//			if ipnet.IP.To4() != nil {
+//				return ipnet.IP
+//			}
+//		}
+//	}
+//	return nil
+//}
+//
+
+//
+//func wasm(writer http.ResponseWriter, request *http.Request) {
+//	writer.Header().Set("Access-Control-Allow-Origin", "*")
+//	http.ServeFile(writer, request, "./assets/simple.wasm")
+//}
+//
+//var upgrader = websocket.Upgrader{
+//	ReadBufferSize:  1024,
+//	WriteBufferSize: 1024,
+//	CheckOrigin:     func(r *http.Request) bool { return true },
+//}
+//
+//func streaming(writer http.ResponseWriter, request *http.Request) {
+//	ws, err := upgrader.Upgrade(writer, request, nil)
+//	if err != nil {
+//		fmt.Println(err)
+//	}
+//	for true {
+//		wsErr := ws.WriteMessage(websocket.BinaryMessage, encodeImage())
+//		if wsErr != nil {
+//			fmt.Println("am i throwing errors")
+//			fmt.Println(err)
+//		}
+//	}
+//}
+//
+//func generateImage(index int) image.Image {
+//	bounds := screenshot.GetDisplayBounds(index)
+//	capturedImg, err := screenshot.CaptureRect(bounds)
+//	if err != nil {
+//		panic(err)
+//	}
+//	return image.Image(capturedImg)
+//}
+//
+//func encodeImage() []byte {
+//	buffer := new(bytes.Buffer)
+//	png.Encode(buffer, generateImage(1))
+//	return buffer.Bytes()
+//}
--- main.go
+++ main.go
@@ -1,14 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"lh-toolkit/internal/client"
+	"lh-toolkit/internal/server"
+	"os"
+)
 
 func main() {
+	args := os.Args
+	isServer := args[1]
+	if isServer == "server" {
+		server.AppServer()
+	} else {
+		client.AppClient()
+	}
 	wait := make(chan struct{})
 	<-wait
 }
 
-//export add
-func add(x, y int) int {
-	fmt.Print("im adding two numbers")
-	return x + y
-}
+//func main() {
+//	wait := make(chan struct{})
+//	<-wait
+//}
+//
+////export add
+//func add(x, y int) int {
+//	fmt.Print("im adding two numbers")
+//	return x + y
+//}
